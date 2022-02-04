85c8820de5464342c129bd983001b2c4f4e7ee51
krolevska <krolevska@gmail.com>
2022-02-04 21:52:31 +0200

    Жопка

--- firstApp/Program.cs
+++ firstApp/Program.cs
@@ -5,7 +5,7 @@ public class MainClass
     public static void Main()
     {
         Console.WriteLine("Законодательство США допускало отправку детей по почте до 1913 года");
-        Console.WriteLine("Ответь на данный факт true или false");
+        Console.WriteLine("Жопка");
         var b = Console.ReadLine();
         Convert.ToBoolean(b);
         Console.WriteLine($"Ответ:{ b}");
