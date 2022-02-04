accd814e16f65d8d8f5666b75934fd5912367626
krolevska <krolevska@gmail.com>
2022-02-04 21:47:07 +0200

    Add project files.

---
+++ firstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        Console.WriteLine("Законодательство США допускало отправку детей по почте до 1913 года");
+        Console.WriteLine("Ответь на данный факт true или false");
+        var b = Console.ReadLine();
+        Convert.ToBoolean(b);
+        Console.WriteLine($"Ответ:{ b}");
+
+    }
+}
