607f303adc0848343f5f518673f9e03126e27e9f
krolevska <krolevska@gmail.com>
2022-02-05 13:33:46 +0200

    task 4

--- firstApp/Program.cs
+++ firstApp/Program.cs
@@ -4,14 +4,8 @@ public class MainClass
 {
     public static void Main()
     {
-        string first = Console.ReadLine();
-        string second = Console.ReadLine();
-        string third = Console.ReadLine();
-        string fourth = Console.ReadLine();
-        float a = float.Parse(first);
-        float b = float.Parse(second);  
-        float c = float.Parse(third);
-        float d = float.Parse(fourth);
-        Console.WriteLine((b + d) / a + c);
+        int a = 0;
+        int b = 4;
+        Console.WriteLine(--a * b);
     }
 }
