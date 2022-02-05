eeec467326a0cdf9f06c40613a57eae2f19cf44e
krolevska <krolevska@gmail.com>
2022-02-05 13:13:49 +0200

    task 3

--- firstApp/Program.cs
+++ firstApp/Program.cs
@@ -4,13 +4,14 @@ public class MainClass
 {
     public static void Main()
     {
-        Console.WriteLine("Введите числа в диапазоне от 1 до 100.");
         string first = Console.ReadLine();
         string second = Console.ReadLine();
-        Console.WriteLine("1 число: " + first);
-        Console.WriteLine("2 число: " + second);
-        double a = double.Parse(first);
-        double b = double.Parse(second);
-        Console.WriteLine(a / b);
+        string third = Console.ReadLine();
+        string fourth = Console.ReadLine();
+        float a = float.Parse(first);
+        float b = float.Parse(second);  
+        float c = float.Parse(third);
+        float d = float.Parse(fourth);
+        Console.WriteLine((b + d) / a + c);
     }
 }
