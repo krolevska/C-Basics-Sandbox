bf77aa253a18dceeb8378e7a728395977aea7fee
krolevska <krolevska@gmail.com>
2022-02-05 22:13:37 +0200

    Task 5

--- firstApp/Program.cs
+++ firstApp/Program.cs
@@ -4,8 +4,13 @@ public class MainClass
 {
     public static void Main()
     {
-        int a = 0;
-        int b = 4;
-        Console.WriteLine(--a * b);
+        Console.WriteLine("Введите число 1:");
+        string a = Console.ReadLine();
+        Console.WriteLine("Введите число 2:");
+        string b = Console.ReadLine();
+        int c = int.Parse(a);
+        int d = int.Parse(b);
+        bool e = ( c <= d );
+        Console.WriteLine($"{c} меньше или равно {d} - {e}");
     }
 }
