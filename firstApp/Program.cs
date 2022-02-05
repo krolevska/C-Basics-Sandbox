3905e3771a20cc9f8da14ddc10ef74bd12261b67
krolevska <krolevska@gmail.com>
2022-02-05 22:32:29 +0200

    random task

--- firstApp/Program.cs
+++ firstApp/Program.cs
@@ -4,13 +4,12 @@ public class MainClass
 {
     public static void Main()
     {
-        Console.WriteLine("Введите число 1:");
-        string a = Console.ReadLine();
-        Console.WriteLine("Введите число 2:");
-        string b = Console.ReadLine();
-        int c = int.Parse(a);
-        int d = int.Parse(b);
-        bool e = ( c <= d );
-        Console.WriteLine($"{c} меньше или равно {d} - {e}");
+        Random rand = new Random();
+        float a = rand.Next(1, 31);
+        float b = rand.Next(1, 31);
+        float c = a + b;
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(a + b);
     }
 }
