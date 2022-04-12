872000a8e0ce21e98d611e2b1fffe6ad6d6bbebb
krolevska <krolevska@gmail.com>
2022-04-12 18:52:47 +0300

    Triangle quiz

--- firstApp/Program.cs
+++ firstApp/Program.cs
@@ -6,16 +6,42 @@ namespace Constant
     {
         static void Main(string[] args)
         {
-            int input = Convert.ToInt32(Console.ReadLine());
-            if (input > 3)
+            int AB, BC, AC;
+            Console.Write("AB = ");
+            AB = Convert.ToInt32(Console.ReadLine());
+            Console.Write("BC = ");
+            BC = Convert.ToInt32(Console.ReadLine());
+            Console.Write("AC = ");
+            AC = Convert.ToInt32(Console.ReadLine());
+            bool realABC = AB + BC >= AC && AC + BC >= AB && AC + AB >= BC;
+            if (realABC)
             {
-                input += 10;     
+                Console.WriteLine("This is a triangle, thanks.");
+                bool squareABC = AB * AB + BC * BC == AC * AC || AC * AC + BC * BC == AB * AB || AC * AC + AB * AB == BC * BC;
+                bool evenABC = AB == AC && AB == BC && AC == BC;
+                bool isoABC = AB == BC || AC == BC || AB == AC;
+                if (squareABC)
+                {
+                    Console.WriteLine("Triangle is square.");
+                }
+                else if (evenABC)
+                {
+                    Console.WriteLine("Triangle is equilateral.");
+                }
+                else if (isoABC)
+                {
+                    Console.WriteLine("Triangle is isosceles.");
+                }
+                else
+                {
+                    Console.WriteLine("This triangle is nor square, neither equilateral, neather isosceles.");
+                }
             }
             else
             {
-                input *= 10;
-            }    
-            Console.WriteLine(input);
+                Console.WriteLine("This is not a triangle.");
+            }
+                        
         }
     }
-}
+ }
