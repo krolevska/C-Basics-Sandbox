875bb818bc4a6a3572ffb087d9400e0c3f52a3c2
krolevska <krolevska@gmail.com>
2022-07-01 18:44:54 +0300

    months

--- firstApp/Program.cs
+++ firstApp/Program.cs
@@ -6,58 +6,48 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            while (true)
-            { 
-                    Console.Write("Give first number: ");
-                    float  A = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Give second number: ");
-                    float B = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Choose an arithmetic operation: +, -, * чи /? ");
-                    string C = Console.ReadLine();
-                    const string SUM = "+", MIN = "-", MULT = "*", DIV = "/";
-                    float RESULT = 0;
-                switch (C)
+            string[] monthNames = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+            int monthIndex = 0;
+            int countAttempts = 0;
+            int availableAttempts = 5;
+
+            while (monthIndex < monthNames.Length)
+            {
+                // звернутися до користувача, щоб він ввів значення місяця по індексу
+                Console.Write($"Please type the name of the month number {monthIndex + 1}: ");
+
+                // користувач вводить значення місяця
+                string monthName = Console.ReadLine();
+
+                // перевіряємо, чи назва місяця співпадає
+                bool monthIsCorect = monthName == monthNames[monthIndex];
+
+
+                // якщо користувач пише правильно, наступна ітерація
+                if (monthIsCorect)
                 {
-                    case SUM:
-                        {
-                            RESULT = A + B;
-                            Console.WriteLine(RESULT);
-                            break;
-                        }
-                    case MIN:
-                        {
-                            RESULT = A - B;
-                            Console.WriteLine(RESULT);
-                            break;
-                        }
-                    case MULT:
-                        {
-                            RESULT = A * B;
-                            Console.WriteLine(RESULT);
-                            break;
-                        }
-                    case DIV:
-                        {
-                            if (B > 0)
-                            {
-                                RESULT = A / B;
-                                Console.WriteLine(RESULT);
-                            }
-                            else
-                            {
-                                RESULT = 0;
-                                Console.WriteLine("Cannot divide by zero. ");
-                            }
-                            break;
-                        }
-                    default:
-                        {
-                            RESULT = 0;
-                            Console.WriteLine("Unknown operation, try again. ");
-                            break;
-                        }
+                    monthIndex++;
+                    continue;
+                }
+
+                // якщо помиляється, повторюється та ж ітерація і росте лічильник спроб
+
+                else
+                {
+                    // якщо менше 6 спроб
+                    countAttempts++;
+                    bool attemptsLeft = countAttempts < availableAttempts;
+                    if (attemptsLeft)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        break;
+                    }
                 }
             }
+
         }
     }
  }
