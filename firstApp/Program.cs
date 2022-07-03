f2975bd241e693cff6160eb48ffc5564245a3e1b
krolevska <krolevska@gmail.com>
2022-07-03 22:31:40 +0300

    fucking matrix

--- firstApp/Program.cs
+++ firstApp/Program.cs
@@ -1,34 +1,87 @@
 ﻿using System;
 
-namespace FuckingArray
+namespace FuckingMatrixMultiplying
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //1.Выведите на экран консоли все числа от 0 до 100;
-            for (int a = 0; a <= 100; a++)
+            //            Напишите программу, в которой создайте два двумерных массива,
+            //            по логике задачи реализующих математические матрицы.
+            Console.WriteLine("Let us multiply two matrixes? ");
+            decimal[,] arrayOne, arrayTwo;
+
+            //            Размерность массивов получите от пользователя.
+            //            Размерность массивов должна быть таковой, чтобы обеспечить возможность умножения этих матриц.
+            //            На это ограничение нужно выполнить проверку.
+            int linesArrayOne, columnsArrayOne, linesArrayTwo, columnsArrayTwo;
+
+            Console.WriteLine("How many lines in first matrix? ");
+            linesArrayOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How many columns in first matrix? ");
+            columnsArrayOne = Convert.ToInt32(Console.ReadLine());
+
+            linesArrayTwo = 0;
+            Console.WriteLine("How many lines in second matrix? ");
+            linesArrayTwo = Convert.ToInt32(Console.ReadLine());
+
+            while (linesArrayTwo != columnsArrayOne)
             {
-                Console.Write(a + " ");
+                Console.WriteLine("Try again");
+                linesArrayTwo = Convert.ToInt32(Console.ReadLine());
             }
-            Console.WriteLine("");
-            //2.Выведите на экран числа от 1001 до 2500 с шагом – 3;
-            for (int b = 1001; b <= 2500; b += 3)
+
+            columnsArrayTwo = 0;
+            Console.WriteLine("How many columns in second matrix? ");
+            columnsArrayTwo = Convert.ToInt32(Console.ReadLine());
+
+            //При прохождении проверки, заполните массивы данными, получая их от пользователя.
+            arrayOne = new decimal[linesArrayOne, columnsArrayOne];
+            arrayTwo = new decimal[linesArrayTwo, columnsArrayTwo];
+
+            for (int i = 0; i < arrayOne.GetLength(0); i++)
             {
-                Console.Write(b + " ");
+                for (int j = 0; j < arrayOne.GetLength(1); j++)
+                {
+                    Console.WriteLine($"What is the {i + 1} number in the {j + 1} column in the first matrix? ");
+                    arrayOne[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
             }
-            //3.Выведите числа от 100 до 0 с шагом – 4;
-            Console.WriteLine("");
-            for (int c = 100; c >= 0; c -=4)
+
+            for (int i = 0; i < arrayTwo.GetLength(0); i++)
             {
-                Console.Write(c + " ");
+                for (int j = 0; j < arrayTwo.GetLength(1); j++)
+                {
+                    Console.WriteLine($"What is the {i + 1} number in the {j + 1} column in the second matrix? ");
+                    arrayTwo[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
             }
-            Console.WriteLine("");
-            //4.Выведите числа от 1 до 5 с шагом 0,2.
-            for (double d = 1; d <= 5; d +=0.2)
+            //foreach (int i in arrayOne)
+            //{
+            //    Console.Write(i);
+            //}
+
+            //foreach (int j in arrayTwo)
+            //{
+            //    Console.Write(j);
+            //}
+
+            //Умножьте матрица
+            decimal arraysMult = 0;
+            for (int i = 0; i < arrayOne.GetLength(0); i++)
             {
-                Console.Write(d + " ");
+                for (int j = 0; j < arrayOne.GetLength(1); j++)
+                {
+                    for (int k = 0; k < arrayTwo.GetLength(1); k++)
+                    {
+                        arraysMult += arrayOne[i, j] * arrayTwo[j, k];
+                    }
+                }    
             }
+            
+            //Выведите на экран результат умножения матриц.
+            Console.WriteLine($"The result is {arraysMult}. ");
         }
     }
 }
