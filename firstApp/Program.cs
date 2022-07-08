0b57174722bad1c549c6451427d5014994cfdd99
krolevska <krolevska@gmail.com>
2022-07-08 18:29:06 +0300

    Salary Calculator / Methods Introduction

--- firstApp/Program.cs
+++ firstApp/Program.cs
@@ -1,30 +1,57 @@
 ﻿using System;
 
-namespace FuckingMatrixMultiplying
+namespace SalaryCalc
 {
     class Program
     {
+        /// <summary>
+        /// Создайте метод, который считает зарплату рабочему, 
+        /// исходя из количества дней и подневной оплаты труда.
+        /// 
+        /// Пользователь вводит количество сотрудников предприятия,
+        /// стоимость 1 трудодня сотрудника и количество дней, за которые производится оплата.
+        /// </summary>
+        /// <returns>quontityEmployee, salary</returns>
+        static (int quontityEmployee, int salary) Input()
+        {
+            Console.WriteLine("How many people work here? ");
+            int quontityEmployee = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How much does 1 working day cost? ");
+            int costPerDay = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many days did you work? ");
+            int workingDays = Convert.ToInt32(Console.ReadLine());
+            int salary = costPerDay * workingDays;
+
+            return (quontityEmployee, salary);
+        }
+        /// <summary>
+        /// Создайте метод, который принимает в себя количество сотрудников,
+        /// количество рабочих дней и цену трудодня,
+        /// и вычисляет общий объем денег, необходимый для оплаты полученному количеству сотрудников,
+        /// используя внутри себя первый метод.
+        /// 
+        /// Посчитайте и выведите на экран общий объем денег,
+        /// необходимый для оплаты зарплаты введенному пользователем количеству сотрудников.
+        /// </summary>
+        /// <returns>total</returns>
+        static int Money()
+        {
+            var input = Input();
+            int quontityEmployee = input.quontityEmployee, salary = input.salary;
+            int total = quontityEmployee * salary;
+            return total;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Give me the text. ");
 
-            string text = Console.ReadLine();
+            var input = Input();
+            int salary = input.salary;
+            Console.WriteLine($"The salary is {salary}");
 
-            string[][] words;
+            int moneyVolume = Money();
+            Console.WriteLine($"We need {moneyVolume} this month");
 
-            string[] sentence = text.Split(".");
-
-            words = new string[sentence.Length][];
-
-            for (int i = 0; i < sentence.Length; i++)
-            {
-                words[i] = sentence[i].Split(" ");
-
-                for (int j = 0; j < words[i].Length; j++)
-                {
-                    Console.WriteLine(words[i][j]);
-                }
-            }
         }
     }
 }
